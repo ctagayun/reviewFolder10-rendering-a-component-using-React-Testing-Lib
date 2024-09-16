5bf7f0b5ade4541537b6313ab4ee4b06cc9cbe2d
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-09-16 09:28:17 -0500

    save

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import * as React from 'react';
-
-const title = 'Hello This is App Demos How to Render a Component using RTL';
-
 function App() {
-  return <div>{title}</div>;
-}
+    const [search, setSearch] = React.useState('');
+  
+    function handleChange(event) {
+      setSearch(event.target.value);
+    }
+  
+    return (
+      <div>
+        <Search value={search} onChange={handleChange}>
+          Search:
+        </Search>
+  
+        <p>Searches for {search ? search : '...'}</p>
+      </div>
+    );
+  }
+  
+  function Search({ value, onChange, children }) {
+    return (
+      <div>
+        <label htmlFor="search">{children}</label>
+        <input
+          id="search"
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </div>
+    );
+  }
+  
+  
 
 export default App;

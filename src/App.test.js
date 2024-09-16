7599fd72644bec4be3e1fc2a14a035107822e1ce
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-09-16 09:27:14 -0500

    save

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import { Counter } from './App';
+import { render, screen } from '@testing-library/react';
 
 /*
   Jest is a test runner, which gives you the ability to run tests with Jest 
@@ -10,47 +8,24 @@ import { Counter } from './App';
   everything needed for now to understand why we need Jest in the first place.
   (see https://www.robinwieruch.de/react-testing-library/)
 */
-// describe('My Test Suite', () => {
-//     it('My Test Case', () => {
-//       expect(true).toEqual(true);
-//     });
-//   });
 
-  describe('true is truthy and false is falsy', () => { //describe-block
-    it('true is truthy', () => {                        //it-block
-      expect(true).toBe(true);
-    });
+//  Defining a Test Suite   
+// describe('my function or component', () => {  <-- describe-block
+//    it('does the following', () => {           <-  it-block or test-case
+
+//    });
+//  });
+
+ 
+  import * as React from 'react';
+  import {render} from 'testing-library/react';
+  import App from './App';
   
-    it('false is falsy', () => {                        //it-block
-      expect(false).toBe(false);
+  //the "render" function takes any JSX as argument to render it as output
+  describe ('App', () =>{
+    it('renders App component', () => { 
+      render(<App />);
+
+      screen.debug(); //will let you see html output of App component
     });
-  });
-
-   //Let's test the function defined inline
-   function add(x, y) {
-    return x + y;
-   }
-  
-   describe('add', () => {
-    it('add up two values', () => {
-      expect(add(2, 4)).toBe(6);
-    });
-  });
-
-    //Let's test a function defined in file math.js called "sum"
-    //To do this import the file in this case.
-    //   describe('sum', () => {
-    //     it('sums up two values', () => {
-    //       expect(sum(2, 4)).toBe(6);
-    //     });
-    //   });
-
-  //Let's test a component call counter which is defined in another file (in this case  App.js).
-  //First import the component here
-  // describe('Counter', () => {
-  //   test('snapshot renders', () => {
-  //     const component = renderer.create(<Counter counter={1} />);
-  //     let tree = component.toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  // });
+  })
